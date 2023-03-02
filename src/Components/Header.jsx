e5aa97753041d8ae43b8e9a9f94f34fe6e7eed7e
Jaime A <jaimeangel1097@gmail.com>
2023-03-01 19:19:06 -0500

    creando primeros componenetes Header/Nuevo presupuesto

---
+++ src/Components/Header.jsx
@@ -0,0 +1,27 @@
+import NuevoPresupuesto from './NuevoPresupuesto';
+import ControlPresupuesto from './ControlPresupuesto';
+
+function Header({presupuesto,setPresupuesto,setIsValidPresupuesto,isValidPresupuesto}) {
+  return (
+    <header>
+      <h1>Planificador de gastos</h1>
+
+      {
+        !isValidPresupuesto ?
+            <NuevoPresupuesto
+                presupuesto={presupuesto}
+                setPresupuesto={setPresupuesto}
+                setIsValidPresupuesto={setIsValidPresupuesto}
+            />
+        :(
+            <ControlPresupuesto
+            presupuesto={presupuesto}
+            />
+        )
+      }
+
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/Components/Modal.jsx
@@ -0,0 +1,10 @@
+function Modal() {
+  return (
+    <div className="modal">
+      <h2>Desde Modal</h2>
+      <h1>Mira</h1>
+    </div>
+  )
+}
+
+export default Modal;
